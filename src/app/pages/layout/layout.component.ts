5ed47eecfc9ddf2b5b172a0d367fc6993a28d44d
tezeethiopia3 <tezeethiopia3@gmail.com>
2024-08-11 18:14:28 +0300

    ffjjjj

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  private route=inject(Router);
+  logout(){
+    localStorage.removeItem("ticketUser");
+    this.route.navigateByUrl("login");
+
+  }
 
 }
